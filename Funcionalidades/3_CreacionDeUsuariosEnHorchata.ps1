9d55e026d98e6e21233c2233536a94b9610148e7
Nois2 <103067863+Nois2@users.noreply.github.com>
2024-08-30 22:06:08 -0600

    Arreglacion de bug 3 (proceso)

--- Funcionalidades/3_CreacionDeUsuariosEnHorchata.ps1
+++ Funcionalidades/3_CreacionDeUsuariosEnHorchata.ps1
@@ -1,95 +1,96 @@
 # Cargar el módulo de Active Directory (si aún no está cargado)
 Import-Module ActiveDirectory
 
-function creacionDeUsuariosCatedra {
-    Read-Host -Prompt "Presiona Enter para Continuar...."
+function CreacionDeUsuariosCatedra {
+    Read-Host -Prompt "Presiona Enter para Continuar..."
 
-    Start-Sleep -Seconds 2
-
+    # Lista de usuarios a crear
+    $usuarios = @(
+        @{
+            Name = "Shaki Mebarack"
+            GivenName = "Shaki"
+            Surname = "Mebarack"
+            SamAccountName = "shaki.mebarack"
+            UserPrincipalName = "shaki.mebarack@horchata.sv"
+            Path = "OU=Compras,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv"
+            Password = "Factura23"
+            EmailAddress = "shaki.mebarack@horchata.sv"
+            Description = "Usuario creado para el departamento de compras."
+        },
+        @{
+            Name = "Rosalia Tabela"
+            GivenName = "Rosalia"
+            Surname = "Tabela"
+            SamAccountName = "rosalia.tabela"
+            UserPrincipalName = "rosalia.tabela@horchata.sv"
+            Path = "OU=Compras,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv"
+            Password = "Motomami23"
+            EmailAddress = "rosalia.tabela@horchata.sv"
+            Description = "Usuario creado para el departamento de compras."
+        },
+        @{
+            Name = "Estudiante01"
+            GivenName = "Estudiante"
+            Surname = "01"
+            SamAccountName = "estudiante01"
+            UserPrincipalName = "estudiante01@horchata.sv"
+            Path = "OU=Ventas,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv"
+            Password = "Password01"
+            EmailAddress = "estudiante01@horchata.sv"
+            Description = "Usuario creado para fines de prueba."
+        },
+        @{
+            Name = "Estudiante02"
+            GivenName = "Estudiante"
+            Surname = "Dos"
+            SamAccountName = "estudiante02"
+            UserPrincipalName = "estudiante02@horchata.sv"
+            Path = "OU=Ventas,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv"
+            Password = "Password02"
+            EmailAddress = "estudiante02@horchata.sv"
+            Description = "Usuario creado como estudiante para el departamento de compras."
+        },
+        @{
+            Name = "Yanira Berrios"
+            GivenName = "Yanira"
+            Surname = "Berrios"
+            SamAccountName = "yanira.berrios"
+            UserPrincipalName = "yanira.berrios@horchata.sv"
+            Path = "OU=Diseño,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv"
+            Password = "Corazonbello"
+            EmailAddress = "yanira.berrios@horchata.sv"
+            Description = "Usuario creado para Yanira Berrios en el departamento de compras."
+        },
+        @{
+            Name = "Estudiante04"
+            GivenName = "Estudiante"
+            Surname = "04"
+            SamAccountName = "estudiante04"
+            UserPrincipalName = "estudiante04@horchata.sv"
+            Path = "OU=Diseño,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv"
+            Password = "Password04"
+            EmailAddress = "estudiante04@horchata.sv"
+            Description = "Usuario creado para Estudiante04 en el departamento de compras."
+        }
+    )
+     
     # Crear usuarios
-    New-ADUser -Name "Shaki Mebarack" `
-        -GivenName "Shaki" `
-        -Surname "Barack" `
-        -SamAccountName "shaki.mebarack" `
-        -UserPrincipalName "shaki.mebarack@horchata.sv" `
-        -Path "OU=Compras,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv" `
-        -AccountPassword (ConvertTo-SecureString "Factura23" -AsPlainText -Force) `
-        -Enabled $true `
-        -Description "Usuario creado para el departamento de compras."
+    foreach ($usuario in $usuarios) {
+        Import-Module ActiveDirectory
+        New-ADUser -Name $usuario.Name `
+            -GivenName $usuario.GivenName `
+            -Surname $usuario.Surname `
+            -SamAccountName $usuario.SamAccountName `
+            -UserPrincipalName $usuario.UserPrincipalName `
+            -Path $usuario.Path `
+            -AccountPassword (ConvertTo-SecureString $usuario.Password -AsPlainText -Force) `
+            -Enabled $true `
+            -Description $usuario.Description
 
-    Set-ADUser -Identity "shaki.mebarack" -EmailAddress "shaki.mebarack@horchata.sv"
+        Set-ADUser -Identity $usuario.SamAccountName -EmailAddress $usuario.EmailAddress
 
-    Start-Sleep -Seconds 2
-
-    New-ADUser -Name "Rosalia Tabela" `
-        -GivenName "Rosalia" `
-        -Surname "Tabela" `
-        -SamAccountName "rosalia.tabela" `
-        -UserPrincipalName "rosalia.tabela@horchata.sv" `
-        -Path "OU=Compras,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv" `
-        -AccountPassword (ConvertTo-SecureString "Motomami23" -AsPlainText -Force) `
-        -Enabled $true `
-        -Description "Usuario creado para el departamento de compras."
-
-    Set-ADUser -Identity "rosalia.tabela" -EmailAddress "rosalia.tabela@horchata.sv"
-
-    Start-Sleep -Seconds 2
-
-    New-ADUser -Name "Estudiante01" `
-        -GivenName "Estudiante" `
-        -Surname "01" `
-        -SamAccountName "estudiante01" `
-        -UserPrincipalName "estudiante01@horchata.sv" `
-        -Path "OU=Ventas,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv" `
-        -AccountPassword (ConvertTo-SecureString "Password01" -AsPlainText -Force) `
-        -Enabled $true `
-        -Description "Usuario creado para fines de prueba."
-
-    Set-ADUser -Identity "estudiante01" -EmailAddress "estudiante01@horchata.sv"
-
-    Start-Sleep -Seconds 2
-
-    New-ADUser -Name "Estudiante02" `
-        -GivenName "Estudiante" `
-        -Surname "Dos" `
-        -SamAccountName "estudiante02" `
-        -UserPrincipalName "estudiante02@horchata.sv" `
-        -Path "OU=Ventas,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv" `
-        -AccountPassword (ConvertTo-SecureString "Password02" -AsPlainText -Force) `
-        -Enabled $true `
-        -Description "Usuario creado como estudiante para el departamento de compras."
-
-    Set-ADUser -Identity "estudiante02" -EmailAddress "estudiante02@horchata.sv"
-
-    Start-Sleep -Seconds 2
-
-    New-ADUser -Name "Yanira Berrios" `
-        -GivenName "Yanira" `
-        -Surname "Berrios" `
-        -SamAccountName "yanira.berrios" `
-        -UserPrincipalName "yanira.berrios@horchata.sv" `
-        -Path "OU=Diseño,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv" `
-        -AccountPassword (ConvertTo-SecureString "Corazonbello" -AsPlainText -Force) `
-        -Enabled $true `
-        -Description "Usuario creado para Yanira Berrios en el departamento de compras."
-
-    Set-ADUser -Identity "yanira.berrios" -EmailAddress "yanira.berrios@horchata.sv"
-
-    Start-Sleep -Seconds 2
-
-    New-ADUser -Name "Estudiante04" `
-        -GivenName "Estudiante" `
-        -Surname "04" `
-        -SamAccountName "estudiante04" `
-        -UserPrincipalName "estudiante04@horchata.sv" `
-        -Path "OU=Diseño,OU=Departamento Comercial,OU=Gerencia General,DC=horchata,DC=sv" `
-        -AccountPassword (ConvertTo-SecureString "Password04" -AsPlainText -Force) `
-        -Enabled $true `
-        -Description "Usuario creado para Estudiante04 en el departamento de compras."
-
-    Set-ADUser -Identity "estudiante04" -EmailAddress "estudiante04@horchata.sv"
-
-    Start-Sleep -Seconds 2
+        Start-Sleep -Seconds 2
+    }
 
     Read-Host -Prompt "Tus Usuarios han sido creados, regresando al menu. Presiona Enter..."
     Start-Sleep -Seconds 2
@@ -97,4 +98,4 @@ function creacionDeUsuariosCatedra {
 }
 
 # Llamar a la función
-creacionDeUsuariosCatedra
+CreacionDeUsuariosCatedra
