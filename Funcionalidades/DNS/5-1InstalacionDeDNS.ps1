6eb5051122d988694a0bdea63109216c5e0b19a0
Nois2 <103067863+Nois2@users.noreply.github.com>
2024-09-04 22:00:18 -0600

    Instalacion dns Fixed

--- Funcionalidades/DNS/5-1InstalacionDeDNS.ps1
+++ Funcionalidades/DNS/5-1InstalacionDeDNS.ps1
@@ -2,19 +2,18 @@ function Test-isInstalledDNSServer{
     $caracteristica = Get-WindowsFeature -Name DNS
     return $caracteristica.Installed
 }
-function instalarDHCPServer{
+function instalacionDNS{
     #Verificar la instalacion de DHCP Server
     if(-not (Test-isInstalledDNSServer)){
         Install-WindowsFeature -Name DNS -IncludeManagementTools -IncludeAllSubFeature       
             Write-Host "Esperando a que la instalación de DNS se complete..."
             Start-Sleep -Seconds 5
-        }
-    }else{
+        }else{
         Write-Host "El Servidor DNS ya está instalado."
         # En caso de que esté instalado, solo se sale de la función
         Read-Host -Prompt "Presione Enter para salir"
         ../mainScript.ps1
         return
     }
-    
 }
+instalacionDNS
