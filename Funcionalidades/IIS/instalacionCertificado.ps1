e521929ac1788f389087862feda786e032539112
NoisFloat <144391743+NoisFloat@users.noreply.github.com>
2024-10-16 01:21:19 -0600

    Prueba final

--- Funcionalidades/IIS/instalacionCertificado.ps1
+++ Funcionalidades/IIS/instalacionCertificado.ps1
@@ -5,22 +5,15 @@ function InstalarCertificadoIIS {
     Import-Module -Name WebAdministration
     
     # Crear un certificado SSL autofirmado para el dominio www.horchata.sv
-    $Cert = New-SelfSignedCertificate -DnsName "www.horchata.sv" `
-        -CertStoreLocation cert:\LocalMachine\My `
-        -KeyLength 2048 `
-        -NotAfter (Get-Date).AddYears(1) `
-        -Subject "CN=www.horchata.sv, O=Horchata SV, C=SV" `
-        -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider (256 bits)"
-    
-    # Configurar el enlace SSL en IIS
-    New-WebBinding -Name "Default Web Site" -Protocol https -Port 443
-    
-    # Agregar el certificado SSL al enlace
-    $certHash = $Cert.Thumbprint
-    $certBinding = "IIS:\SslBindings\0.0.0.0!443"
-    
-    # Vincular el certificado al puerto 443
-    New-Item -Path $certBinding -Value $certHash -Force
+    Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+    New-SelfSignedCertificate -DnsName "www.horchata.sv" -CertStoreLocation "cert:\LocalMachine\My" 
+
+    $cert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like "*www.horchata.sv*"}
+
+    New-WebBinding -Name "www.horchata.sv" -Protocol "https" -Port 443 -IPAddress * -SslFlags 1 -HostHeader "www.horchata.sv"
+
+    (Get-WebBinding -Name "www.horchata.sv" -Protocol "https").AddSslCertificate($cert.Thumbprint, "My")
     
     Write-Host "Certificado SSL para www.horchata.sv instalado y configurado en IIS."
     Start-Sleep -Seconds 1
